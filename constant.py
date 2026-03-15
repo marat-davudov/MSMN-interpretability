64ab423ea2309c8e5d4c2e0356f31761bc80801a
Marat Davudov <marat.m.davudov@gmail.com>
2026-03-15 21:06:21 +0000

    feat: initialise poetry and update constants.py

--- constant.py
+++ constant.py
@@ -1,8 +1,11 @@
 import os
 
 
-# change DATA_DIR for yourself
-# DATA_DIR = "./sample_data/"
-# UMLS_PATH = "umls_path"
-MIMIC_2_DIR = os.path.join(DATA_DIR, "mimic2")
-MIMIC_3_DIR = os.path.join(DATA_DIR, "mimic3")
+# # change DATA_DIR for yourself
+# # DATA_DIR = "./sample_data/"
+# # UMLS_PATH = "umls_path"
+# MIMIC_2_DIR = os.path.join(DATA_DIR, "mimic2")
+# MIMIC_3_DIR = os.path.join(DATA_DIR, "mimic3")
+CHECKPOINTS_DIR = "./checkpoints"
+EMBEDDING_MODEL_PATH = os.path.join(CHECKPOINTS_DIR, "embedding", "word2vec_sg0_100.model")
+MSMN_MODEL_PATH = os.path.join(CHECKPOINTS_DIR, "MSMN", "mimic3-50.pth")
